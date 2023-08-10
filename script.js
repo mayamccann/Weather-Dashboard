b4b5f9cc64553d0646e87f1c285e83b510d89070
Maya McCann <mccannmaya@gmail.com>
2023-08-10 15:37:54 -0400

    Search Value for Weather Function and Weather Forecast

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ $(document).ready(function() {
 $("#search-button").on("click", function () {
 
 var searchTerm = $("#search-value").val();
+
+$("search-value").val ("");
+weatherfunction (searchTerm);
+weatherforecast (searchTerm);
+}) ;
